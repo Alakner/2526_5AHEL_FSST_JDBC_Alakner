92a993431866925795ad99eaae4f6f58f68f98d0
Alakner <alakner@htl-steyr.ac.ac>
2026-01-15 11:18:02 +0100

    Fixed Errors

--- JDBCDatenvisualisierung/src/main/java/org/example/jdbcdatenvisualisierung/JDBCApplication.java
+++ JDBCDatenvisualisierung/src/main/java/org/example/jdbcdatenvisualisierung/JDBCApplication.java
@@ -4,17 +4,15 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.io.IOException;
-
-
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import java.io.IOException;
 import java.sql.*;
+import java.util.ArrayList;
 
-
-public class Continent from ArrayList<Continent>{
+class Continent {
     String name;
     int population;
 }
@@ -23,33 +21,26 @@ public class JDBCApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        Continent continents = new Continent();
+        ArrayList<Continent> continents = new ArrayList<>();
         String url = "jdbc:postgresql://xserv:5432/world2";
         String user = "reader";
         String password = "reader";
 
         try {
-
-            // 1. Establish Connection
             Connection con = DriverManager.getConnection(url, user, password);
-
-            // 3. Create Statement
             Statement stmt = con.createStatement();
-
-            // 4. Execute Query
             ResultSet rs = stmt.executeQuery(
                     "SELECT country.continent, SUM(country.population) AS bevoelkerung\n" +
                             "FROM country\n" +
                             "GROUP BY country.continent;");
 
-            // 5. Process Results
             while (rs.next()) {
-                continents.add(new Continent());
-                continents.get(continents.size() - 1).name = rs.getString("continent");
-                continents.get(continents.size() - 1).population = rs.getInt("bevoelkerung");
+                Continent c = new Continent();
+                c.name = rs.getString("continent");
+                c.population = rs.getInt("bevoelkerung");
+                continents.add(c);
             }
 
-            // 6. Close resources
             rs.close();
             stmt.close();
             con.close();
@@ -57,22 +48,18 @@ public class JDBCApplication extends Application {
             e.printStackTrace();
         }
 
-
-        Axis continentAxis = new CategoryAxis();
-        Axis populationAxis = new NumberAxis();
-        BarChart barChart = new BarChart();
+        BarChart<String, Number> barChart = new BarChart<>(new CategoryAxis(), new NumberAxis());
         barChart.setTitle("Population by Continent");
-        continentAxis.setLabel("Continent");
-        populationAxis.setLabel("Population");
+        barChart.getXAxis().setLabel("Continent");
+        barChart.getYAxis().setLabel("Population");
 
-        XYChart.Series dataSeries = new XYChart.Series();
+        XYChart.Series<String, Number> dataSeries = new XYChart.Series<>();
         for (Continent continent : continents) {
-            dataSeries.getData().add(new XYChart.Data(continent.name, continent.population));
+            dataSeries.getData().add(new XYChart.Data<>(continent.name, continent.population));
         }
         barChart.getData().add(dataSeries);
 
-
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(JDBCApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 400);
         stage.setTitle("World 2 DB Visualization");
         stage.setScene(scene);
